8340f441d2ef87db28225ed1585718ba0c5d1fa2
Yuki Itoh <yukiitohand@gmail.com>
2022-05-20 14:57:20 -0400

    startup_addpath is improved

--- cntrmvl_toolbox_startup_addpath_R2019a.m
+++ cntrmvl_toolbox_startup_addpath_R2019a.m
@@ -1,13 +1,13 @@
-function [] = cntrmvl_toolbox_startup_addpath(error_if_not_unique,silent_if_not_unique)
-% cntrmvl_toolbox_startup_addpath
+function [] = cntrmvl_toolbox_startup_addpath_R2019a(varargin)
+% cntrmvl_toolbox_startup_addpath_R2019a
 %  Add paths of continuumRemoval toolbox, while solving dependent toolboxes.
 %  Dependent toolboxes: 
 %     base
 % 
 %  USAGE
-%  >> cntrmvl_toolbox_startup_addpath()
-%  >> cntrmvl_toolbox_startup_addpath(error_if_not_unique)
-%  >> cntrmvl_toolbox_startup_addpath(error_if_not_unique,silent_if_not_unique)
+%  >> cntrmvl_toolbox_startup_addpath_R2019a()
+%  >> cntrmvl_toolbox_startup_addpath_R2019a(error_if_not_unique)
+%  >> cntrmvl_toolbox_startup_addpath_R2019a(error_if_not_unique,silent_if_not_unique)
 %  
 % Optional Input Arguments
 % error_if_not_unique: boolean, (default) false
@@ -18,14 +18,23 @@ function [] = cntrmvl_toolbox_startup_addpath(error_if_not_unique,silent_if_not_
 %   found or multiple versions are detected. If error_if_not_unique, then
 %   this does not have any effect (since error will be thrown.)
 %
-% Only supports MATLAB versions since R2019b
+% Also supports MATLAB versions < R2019b
 % 
 
-% arguments block only supported versions since R2019b
-arguments
-    error_if_not_unique  (1,1) {mustBeMember(error_if_not_unique,[0,1])}  = false;
-    silent_if_not_unique (1,1) {mustBeMember(silent_if_not_unique,[0,1])} = false;
-end
+error_if_not_unique_default  = false;
+silent_if_not_unique_default = false;
+
+p = inputParser;
+p.FunctionName = mfilename;
+p.addOptional('error_if_not_unique' ,error_if_not_unique_default, ...
+    @(x)validateattributes(x,'numeric',{'scalar','binary'},1));
+p.addOptional('silent_if_not_unique',silent_if_not_unique_default, ...
+    @(x)validateattributes(x,'numeric',{'scalar','binary'},2));
+
+parse(p,varargin{:});
+
+error_if_not_unique  = p.Results.error_if_not_unique;
+silent_if_not_unique = p.Results.silent_if_not_unique;
 
 %% Automatically find the path to toolboxes
 fpath_self = mfilename('fullpath');
